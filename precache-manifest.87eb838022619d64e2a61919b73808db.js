35f434d40f29574d6e77e0f7280a40c79207adf1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-10 10:43:39 +0000

    Deploy bailly.app to github.com/antoineboquet/bailly.app.git:gh-pages

---
+++ precache-manifest.87eb838022619d64e2a61919b73808db.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ca8588b2a8fa914e69487dbce4ecad",
+    "url": "/404.html"
+  },
+  {
+    "revision": "eb1e647870add0502f8f010b19de32af",
+    "url": "/COPYING"
+  },
+  {
+    "revision": "b8cae78a7c17580a4ea2",
+    "url": "/css/about.a1c0aa5c.css"
+  },
+  {
+    "revision": "0b1be5f4eb9e8b485118",
+    "url": "/css/app.20fd0afc.css"
+  },
+  {
+    "revision": "a95164c213454e7a96d1",
+    "url": "/css/definition.6f0b7725.css"
+  },
+  {
+    "revision": "c30fcbe24fd7e440bc77",
+    "url": "/css/search.ebfc786c.css"
+  },
+  {
+    "revision": "2036b5c318fcab4078c8",
+    "url": "/css/settings.49527799.css"
+  },
+  {
+    "revision": "6899d8b33fb59c1c2015d608622f2d42",
+    "url": "/fonts/GFSDidot-Bold.6899d8b3.woff2"
+  },
+  {
+    "revision": "9386decee8a5e827e358a3b043f21e11",
+    "url": "/fonts/GFSDidot.9386dece.woff2"
+  },
+  {
+    "revision": "1ef3810b39186217c92123a124333276",
+    "url": "/fonts/IFAOGrec.1ef3810b.woff2"
+  },
+  {
+    "revision": "bf1f5ed9b8915dce23b16c9c234f658d",
+    "url": "/fonts/IFAOGrecBold.bf1f5ed9.woff2"
+  },
+  {
+    "revision": "99cd261de7e51f95980c6cee4a63d894",
+    "url": "/fonts/LibertinusSerif-Bold.99cd261d.woff2"
+  },
+  {
+    "revision": "593a9690f85b3d72e6703f6ea8d0253b",
+    "url": "/fonts/LibertinusSerif-Italic.593a9690.woff2"
+  },
+  {
+    "revision": "1b502cb51b69593ab54e05a3848f10a2",
+    "url": "/fonts/LibertinusSerif.1b502cb5.woff2"
+  },
+  {
+    "revision": "18ee0d0af8b384cac85c606c16d25c1a",
+    "url": "/fonts/NotoSans.18ee0d0a.woff2"
+  },
+  {
+    "revision": "96aea5bf61ca22cda01b9b6c10ba8dec",
+    "url": "/fonts/Quicksand-Bold.96aea5bf.woff2"
+  },
+  {
+    "revision": "a16ab174e4891d3f9a188347ce61d1ae",
+    "url": "/fonts/Quicksand.a16ab174.woff2"
+  },
+  {
+    "revision": "35915e2c9e96410d9aa08f6c64243d0a",
+    "url": "/img/abréviations-signes-usuels.jpg"
+  },
+  {
+    "revision": "8e20189857f90c14a5f47109ca61a3cb",
+    "url": "/img/bailly-illustration.8e201898.jpg"
+  },
+  {
+    "revision": "d00a9beb9658148e2c3bf73e61e200af",
+    "url": "/img/liste-auteurs-ouvrages.jpg"
+  },
+  {
+    "revision": "e373e42020159d1d7ecda0d55b581fe5",
+    "url": "/img/logo-background.e373e420.jpg"
+  },
+  {
+    "revision": "c6759ccb17a8295d45027a1b65c93ed2",
+    "url": "/img/mesures.jpg"
+  },
+  {
+    "revision": "a7104b650183700ed8cc16f7d3ea67ad",
+    "url": "/img/notice-édition-2020.jpg"
+  },
+  {
+    "revision": "a2ec75bf020034a97ce34843f95f3325",
+    "url": "/img/préface-anatole-bailly.jpg"
+  },
+  {
+    "revision": "e5744722302bc833c3ba75600b556f58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8cae78a7c17580a4ea2",
+    "url": "/js/about.bcf9dfae.js"
+  },
+  {
+    "revision": "0b1be5f4eb9e8b485118",
+    "url": "/js/app.efd5e45b.js"
+  },
+  {
+    "revision": "d61081f07ec9a90f5acb",
+    "url": "/js/chunk-vendors.b58e0705.js"
+  },
+  {
+    "revision": "a95164c213454e7a96d1",
+    "url": "/js/definition.f6435a29.js"
+  },
+  {
+    "revision": "c30fcbe24fd7e440bc77",
+    "url": "/js/search.e7d8bafe.js"
+  },
+  {
+    "revision": "2036b5c318fcab4078c8",
+    "url": "/js/settings.f0773675.js"
+  },
+  {
+    "revision": "ab55f4a06b8808397899829f215bcb4d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "40a519c7b9b9ac38ca4e6b5eb6754d3b",
+    "url": "/pdf/abréviations-signes-usuels.pdf"
+  },
+  {
+    "revision": "d8727b577a44365d4f683ba1bf6e4a00",
+    "url": "/pdf/liste-auteurs-ouvrages.pdf"
+  },
+  {
+    "revision": "19dbe6a371c77ba650c6936505c732ff",
+    "url": "/pdf/mesures.pdf"
+  },
+  {
+    "revision": "f202d73df13c8b097e5121d714efd6b3",
+    "url": "/pdf/notice-édition-2020.pdf"
+  },
+  {
+    "revision": "858866eb32522bbed92090fbeab55706",
+    "url": "/pdf/préface-anatole-bailly.pdf"
+  },
+  {
+    "revision": "d4e2699ce31da7123598646989664275",
+    "url": "/robots.txt"
+  }
+]);
---
+++ precache-manifest.ba96b4b2ea563bdb623b13e3698eb861.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac5956ea115527131ff",
+    "url": "/css/about.a1c0aa5c.css"
+  },
+  {
+    "revision": "f429934c80e18d4ef00c",
+    "url": "/css/app.20fd0afc.css"
+  },
+  {
+    "revision": "b2d2437aca61baa46476",
+    "url": "/css/definition.6f0b7725.css"
+  },
+  {
+    "revision": "45effbb5179f53445a89",
+    "url": "/css/search.ebfc786c.css"
+  },
+  {
+    "revision": "bf6d2cb95fc3738ef409",
+    "url": "/css/settings.49527799.css"
+  },
+  {
+    "revision": "6899d8b33fb59c1c2015d608622f2d42",
+    "url": "/fonts/GFSDidot-Bold.6899d8b3.woff2"
+  },
+  {
+    "revision": "9386decee8a5e827e358a3b043f21e11",
+    "url": "/fonts/GFSDidot.9386dece.woff2"
+  },
+  {
+    "revision": "1ef3810b39186217c92123a124333276",
+    "url": "/fonts/IFAOGrec.1ef3810b.woff2"
+  },
+  {
+    "revision": "bf1f5ed9b8915dce23b16c9c234f658d",
+    "url": "/fonts/IFAOGrecBold.bf1f5ed9.woff2"
+  },
+  {
+    "revision": "99cd261de7e51f95980c6cee4a63d894",
+    "url": "/fonts/LibertinusSerif-Bold.99cd261d.woff2"
+  },
+  {
+    "revision": "593a9690f85b3d72e6703f6ea8d0253b",
+    "url": "/fonts/LibertinusSerif-Italic.593a9690.woff2"
+  },
+  {
+    "revision": "1b502cb51b69593ab54e05a3848f10a2",
+    "url": "/fonts/LibertinusSerif.1b502cb5.woff2"
+  },
+  {
+    "revision": "18ee0d0af8b384cac85c606c16d25c1a",
+    "url": "/fonts/NotoSans.18ee0d0a.woff2"
+  },
+  {
+    "revision": "96aea5bf61ca22cda01b9b6c10ba8dec",
+    "url": "/fonts/Quicksand-Bold.96aea5bf.woff2"
+  },
+  {
+    "revision": "a16ab174e4891d3f9a188347ce61d1ae",
+    "url": "/fonts/Quicksand.a16ab174.woff2"
+  },
+  {
+    "revision": "8e20189857f90c14a5f47109ca61a3cb",
+    "url": "/img/bailly-illustration.8e201898.jpg"
+  },
+  {
+    "revision": "e373e42020159d1d7ecda0d55b581fe5",
+    "url": "/img/logo-background.e373e420.jpg"
+  },
+  {
+    "revision": "15148de1ef4485869dedfbadd1aafd25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ac5956ea115527131ff",
+    "url": "/js/about-legacy.3ac5287d.js"
+  },
+  {
+    "revision": "f429934c80e18d4ef00c",
+    "url": "/js/app-legacy.c7310919.js"
+  },
+  {
+    "revision": "5e07cba0b0c63a405f30",
+    "url": "/js/chunk-vendors-legacy.f52b47d5.js"
+  },
+  {
+    "revision": "b2d2437aca61baa46476",
+    "url": "/js/definition-legacy.6d96c2df.js"
+  },
+  {
+    "revision": "45effbb5179f53445a89",
+    "url": "/js/search-legacy.1ca1f0d9.js"
+  },
+  {
+    "revision": "bf6d2cb95fc3738ef409",
+    "url": "/js/settings-legacy.eca3493c.js"
+  },
+  {
+    "revision": "ab55f4a06b8808397899829f215bcb4d",
+    "url": "/manifest.json"
+  }
+]);
