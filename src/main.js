0aed61487ad3e8a5cf203ca12e5df849ca3ff73f
antoineboquet <antoineboquet@users.noreply.github.com>
2022-08-15 11:46:41 +0200

    Suppression du `service worker`

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import SimpleAnalytics from 'simple-analytics-vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
