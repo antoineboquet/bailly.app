1037aab19730984f1382f9dfefd3b37341237317
antoineboquet <antoineboquet@users.noreply.github.com>
2023-04-22 12:04:53 +0200

    Suppression de Simple Analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import SimpleAnalytics from 'simple-analytics-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -8,10 +7,5 @@ import '@/styles/app.scss'
 
 Vue.config.productionTip = false
 Vue.config.ignoredElements = ['ion-icon']
-
-Vue.use(SimpleAnalytics, {
-  domain: 'data.bailly.app',
-  skip: process.env.NODE_ENV !== 'production'
-})
 
 new Vue({ router, store, render: h => h(App) }).$mount('#app')
