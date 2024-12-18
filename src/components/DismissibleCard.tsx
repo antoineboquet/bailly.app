31e2a6c95f03b4604bd6d6708daab8a940fd3203
antoineboquet <4170700+antoineboquet@users.noreply.github.com>
2024-12-18 23:56:14 +0100

    Make the title optional

--- src/components/DismissibleCard.tsx
+++ src/components/DismissibleCard.tsx
@@ -3,7 +3,7 @@ import { LocalStorageKey } from "../enums";
 import Icon, { type IconName } from "./Icon";
 
 interface Props {
-  title: string;
+  title?: string;
   content: JSX.Element;
   icon?: IconName;
   key?: LocalStorageKey;
@@ -30,25 +30,30 @@ export default function DismissibleCard(props: Props) {
   return (
     <Show when={showDismissibleCard()}>
       <article class={`${props.class} select-none rounded-lg bg-secondary-200 p-3 shadow-lg shadow-black/5 dark:bg-secondary-950`}>
-        <header class="mb-3 flex items-center">
-          <h2 class="flex flex-1 items-center font-sans text-xl font-semibold text-secondary-700 lg:text-2xl dark:text-secondary-400">
-            <Show when={props.icon}>
-              <Icon name={props.icon!} class="mr-3" solid={true} />
-            </Show>
-            {props.title}
-          </h2>
-
-          <Icon
-            name="XCircle"
-            class="cursor-pointer fill-secondary-700 hover:scale-105 hover:fill-secondary-800 active:scale-105 dark:fill-secondary-400 dark:hover:fill-secondary-300"
-            solid={true}
-            onClick={dismissCard}
-          />
-        </header>
+        <Icon
+          name="XCircle"
+          class="cursor-pointer float-right fill-secondary-700 hover:scale-105 hover:fill-secondary-800 active:scale-105 dark:fill-secondary-400 dark:hover:fill-secondary-300"
+          solid={true}
+          onClick={dismissCard}
+        />
+        
+        <Show when={props.title}>
+          <header class="mb-3 flex items-center">
+            <h2 class="flex flex-1 items-center font-sans text-xl font-semibold text-secondary-700 lg:text-2xl dark:text-secondary-400">
+              <Show when={props.icon}>
+                <Icon name={props.icon!} class="mr-3" solid={true} />
+              </Show>
+              {props.title}
+            </h2>
+          </header>
+        </Show>
 
         <div
-          class={`space-y-1.5 font-sans font-semibold text-secondary-700 ${props.icon ? "sm:ml-9" : ""} dark:text-secondary-400`}
+          class={`space-y-1.5 font-sans font-semibold text-secondary-700 ${props.icon && props.title ? "sm:ml-9" : ""} dark:text-secondary-400`}
         >
+          <Show when={props.icon && !props.title}>
+            <Icon name={props.icon!} class="inline-flex size-5 mr-1.5" solid={true} />
+          </Show>
           {props.content}
         </div>
       </article>
